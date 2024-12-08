66e42992c3cc51c772a7879dc1759d8682d6ccb8
nathanaday <nraday1221@gmail.com>
2024-12-08 09:55:57 -0800

    Tailwind vue app for edge app web page

---
+++ edge_apps/base/frontend/web-ui/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="flex gap-[5%] flex-wrap">
+    <div class="w-full h-[5%]">
+      <h1>Image Inference Test</h1>
+    </div>
+
+    <!-- Sidebar -->
+    <div class="w-1/4 h-3/4">
+      <div class="card bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">Card title!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END Sidebar -->
+
+    <!-- Content -->
+    <div class="grow h-3/4">
+      <div class="card bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">Card title!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END Content -->
+
+    <!-- Footer -->
+    <div class="w-full h-[5%]">
+      <div class="card bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">Card title!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END Footer -->
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
